c60dee47254f7e05e7289e767c56e9b168980367
Srishti-ii <srishtisrivastava.1291@gmail.com>
2025-09-21 10:47:20 +0000

    operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,61 @@
+let score="33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinnum=Number(score)
+//console.log(typeof valueinnum);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isloggedin="juhi";
+let booleanisloggedin= Boolean(isloggedin)
+//console.log(booleanisloggedin );
+
+//1=>true
+//0=>false
+//""=>false
+//"srishti"=>true
+
+let num=34;
+//let stringnum=String(num)
+//console.log(typeof stringnum);\
+
+//*************************operations**************************
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" juhi"
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let count=100;
+++count;
+console.log(count);
+
+
+
+
+
+
+
