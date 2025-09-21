710fa890f6d710b8f96af3ce78748fc5c24cc203
Srishti-ii <srishtisrivastava.1291@gmail.com>
2025-09-21 10:31:03 +0000

    3rd commit

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score="33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinnum=Number(score)
+//console.log(typeof valueinnum);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isloggedin="juhi";
+let booleanisloggedin= Boolean(isloggedin)
+console.log(booleanisloggedin );
+
+//1=>true
+//0=>false
+//""=>false
+//"srishti"=>true
+
+let num=33;
+let stringnum=String(num)
+console.log(typeof stringnum);
